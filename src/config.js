399ae2869b3687739b51ae915dbc099c4db087c9
Pharaon3 <Pharaon313@outlook.com>
2023-02-21 03:13:32 +0900

    blog + firebase

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import firebase from "firebase/compat";
 import { getFirestore } from "firebase/firestore";
@@ -8,12 +7,10 @@ import { getMessaging } from "firebase/messaging";
 export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const fireDb = firebase.initializeApp(firebaseConfig);
